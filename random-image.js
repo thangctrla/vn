e72dc1d8d06f65443006b96f8d1332169490f7f1
Do Manh Thang <55195341+thangctrla@users.noreply.github.com>
2023-06-01 10:28:46 +0700

    Update random-image.js

--- random-image.js
+++ random-image.js
@@ -1,10 +1,17 @@
+// Định nghĩa các chuỗi s1, s2, s3
+var s1 = "Chuỗi 1";
+var s2 = "Chuỗi 2";
+var s3 = "Chuỗi 3";
+
+// Hàm trả về một chuỗi ngẫu nhiên từ s1, s2, s3
 function getRandomString() {
   const strings = [s1, s2, s3];
   const randomIndex = Math.floor(Math.random() * strings.length);
   return strings[randomIndex];
 }
 
-// Sử dụng hàm để lấy chuỗi ngẫu nhiên
+// Gọi hàm để lấy chuỗi ngẫu nhiên
 const randomString = getRandomString();
 
-console.log(randomString); // In chuỗi ngẫu nhiên ra màn hình console
+// In chuỗi ngẫu nhiên ra màn hình
+console.log(randomString);
