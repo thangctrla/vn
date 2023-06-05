3123c9cb0ebe0994833efefcf37bdcbed1472ae8
Do Manh Thang <55195341+thangctrla@users.noreply.github.com>
2023-06-05 16:27:42 +0700

    Update random-image.js

--- random-image.js
+++ random-image.js
@@ -1,37 +1,63 @@
-// Tạo một phần tử div mới cho popup
-let popup = document.createElement("div");
+// Tạo các phần tử DOM
+const container = document.createElement("div");
+container.className = "container";
 
-// Thêm nội dung và thuộc tính CSS vào phần tử div
-popup.innerHTML = "<h1> day la popup da chinh sua" + new Date() + "</h1>";
-popup.style.position = "fixed";
-popup.style.top = "50%";
-popup.style.left = "50%";
-popup.style.transform = "translate(-50%, -50%)";
-popup.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-popup.style.color = "white";
-popup.style.padding = "20px";
-popup.style.borderRadius = "10px";
-popup.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.3)";
-popup.style.zIndex = "9999";
+const popup = document.createElement("div");
+popup.className = "popup";
 
-// Tạo một phần tử button cho nút "Close"
-let closeButton = document.createElement("button");
-closeButton.innerHTML = "Close";
-closeButton.style.position = "absolute";
-closeButton.style.top = "10px";
-closeButton.style.right = "10px";
-closeButton.style.backgroundColor = "transparent";
-closeButton.style.color = "white";
-closeButton.style.border = "none";
-closeButton.style.cursor = "pointer";
+const popupContent = document.createElement("div");
+popupContent.className = "popup-content";
 
-// Xử lý sự kiện khi người dùng nhấp vào nút "Close"
-closeButton.addEventListener("click", function() {
-  document.body.removeChild(popup);
-});
+const popupTitle = document.createElement("h2");
+popupTitle.className = "popup-title";
+popupTitle.textContent = "Awesome!";
 
-// Gắn nút "Close" vào phần tử div
-popup.appendChild(closeButton);
+const popupBody = document.createElement("p");
+popupBody.className = "popup-body";
+popupBody.textContent =
+  "You just found my pastel gradient button.\nNow, let's see what else it does. :P";
 
-// Gắn phần tử div vào body của trang web
-document.body.appendChild(popup);
+// Gắn các phần tử vào DOM
+popupContent.appendChild(popupTitle);
+popupContent.appendChild(popupBody);
+popup.appendChild(popupContent);
+container.appendChild(popup);
+document.body.appendChild(container);
+
+// Áp dụng CSS bằng JavaScript
+container.style.display = "flex";
+container.style.webkitDisplay = "box";
+container.style.mozDisplay = "box";
+container.style.msDisplay = "flexbox";
+container.style.webkitDisplay = "flex";
+container.style.flexWrap = "wrap";
+container.style.justifyContent = "center";
+container.style.alignContent = "center";
+container.style.padding = "6%";
+container.style.margin = "0";
+
+popup.style.display = "flex";
+popup.style.alignContent = "center";
+popup.style.justifyContent = "center";
+popup.style.backgroundColor = "#fff";
+popup.style.borderRadius = "6px";
+popup.style.padding = "3rem 2rem";
+popup.style.boxShadow = "0 10px 40px -14px rgba(0,0,0,0.25)";
+popup.style.textAlign = "center";
+
+popupContent.style.display = "flex";
+popupContent.style.flexWrap = "wrap";
+popupContent.style.flexDirection = "column";
+
+popupTitle.style.color = "#32325d";
+popupTitle.style.fontSize = "1.8em";
+popupTitle.style.lineHeight = "1.5em";
+popupTitle.style.fontWeight = "900";
+popupTitle.style.marginTop = "0";
+
+popupBody.style.fontSize = "1.1em";
+popupBody.style.lineHeight = "1.6em";
+popupBody.style.color = "#6b7c93";
+popupBody.style.fontWeight = "500";
+popupBody.style.marginBottom = "2.1em";
+popupBody.style.textAlign = "left";
