436dd5f84dd1da3e9f1384da4fab4f2a88e4bf4e
Do Manh Thang <55195341+thangctrla@users.noreply.github.com>
2023-06-07 08:49:08 +0700

    Update random-image.js

--- random-image.js
+++ random-image.js
@@ -6,19 +6,24 @@ popup.innerHTML = `
   <div class="container-popup">
     <div class="cookiesContent" id="cookiesPopup">
       <button class="close-popup">✖</button>
-      <img src="https://cdn-icons-png.flaticon.com/512/9518/9518021.png" alt="cookies-img" />
+      <img src="https://cdn-icons-png.flaticon.com/512/4477/4477205.png" alt="cookies-img" />
       <span>
       <b>Dịch vụ giao hàng</b>
    
     <p>
     Từ 0 => 50k: <b>Miễn phí 1.2km</b>
-    <br>Với mỗi 50k giá trị đơn hàng tiếp theo <b>miễn phí thêm</b> 1km
+    <br>Mỗi 50k giá trị đơn hàng tiếp theo <b>miễn phí thêm</b> 1.0km
+    <br>
+    <span  class="note-popup">* Với km tiếp theo cửa hàng sẽ phụ thu 5k/1km</span>
     </p>
+
     
-   <p>Tính từ <a href="https://goo.gl/maps/z989yV79G56UA3D99">nhà văn hoá tổ dân phố 21</a>
+    <p>Tính từ <a href="https://goo.gl/maps/z989yV79G56UA3D99">nhà văn hoá tổ dân phố 21</a>
+   
+    
     <br>
     Ship tối thiểu: 30k</p>
-    </span><button class="accept-popup"> Đồng ý </button>
+    </span><button class="accept-popup">Đồng ý</button>
     </div>
   </div>
 `;
@@ -56,19 +61,26 @@ popup.querySelector(".close-popup").style.backgroundColor = "transparent";
 popup.querySelector(".close-popup").style.border = "none";
 popup.querySelector(".close-popup").style.marginBottom = "10px";
 
+
+popup.querySelector(".note-popup").style.fontSize = "10px";
+popup.querySelector(".note-popup").style.color = "#838878";
+
+
+
+
 popup.querySelector("img").style.width = "82px";
 popup.querySelector("img").style.marginBottom = "15px";
 
-
+popup.querySelector("b").style.fontSize = "25px";
 popup.querySelector("a").style.textDecoration = "none";
 popup.querySelector("a").style.color = "#ff070b";
 
-popup.querySelector("b").style.fontSize = "25px";
+
+
 
 popup.querySelector("p").style.marginBottom = "40px";
 popup.querySelector("p").style.fontSize = "18px";
 popup.querySelector("p").style.lineHeight = "30px";
-
 popup.querySelector(".accept-popup").style.backgroundColor = "#ed6755";
 popup.querySelector(".accept-popup").style.border = "none";
 popup.querySelector(".accept-popup").style.borderRadius = "5px";
@@ -80,10 +92,9 @@ popup.querySelector(".accept-popup").style.boxShadow = "0px 6px 18px -5px rgba(2
 
 // Gán sự kiện click cho nút đóng
 popup.querySelector(".close-popup").addEventListener("click", function () {
-    popup.style.display = "none"
+  popup.style.display = "none"
 });
+// Gán sự kiện click cho nút đóng
 popup.querySelector(".accept-popup").addEventListener("click", function () {
-    popup.style.display = "none"
+  popup.style.display = "none"
 });
-
-
